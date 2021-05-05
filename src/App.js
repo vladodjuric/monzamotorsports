a062110c7a4bac6847ec98f8944e76cdaea73081
vlado <vladodjuric9@gmail.com>
2021-05-04 22:16:35 -0400

    initial changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Monza Motorsporets</h1>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <h3>Cars</h3>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Monza Motorsports is a boutique-style vehicle reseller specializing in
+          premium imports. We aim to provide an unmatched experience to our
+          clients and our vehicles are meticulously reconditioned to the highest
+          standards. We offer customizable leasing and financing options on on
+          all our vehicles.
+        </p>
+        <h3>Personalization</h3>
+        <p>
+          We offer a full spectrum of vehicle reconditioning and personalization
+          options. From window tinting to custom wrapping to protective film
+          installation, all our products and services adhere to the highest
+          standards of quality and craftsmanship. Rest assured that we only use
+          the best supplies and equipment.
+        </p>
+        <h3>About</h3>
+        <p>
+          Since 2012 Monza Motorsports has been the leading destination for
+          premium imported vehicles. We do things differently by marketing
+          chiefly through personal referrals as we seek to cultivate life-long
+          clients. Professionalism and transparency are our operating principles
+          while delivering real value to our clients is our ultimate goal.
+          Contact us today to start your journey.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
+          className="App-Link"
+          href="https://www.facebook.com/MonzaMotorsports/"
+          target="blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Inventory
         </a>
+        <h3>Contact</h3>
       </header>
     </div>
   );
